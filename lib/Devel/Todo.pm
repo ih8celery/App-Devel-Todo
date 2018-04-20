dc17a9df8351db677095e2e91be6c0bc7cc86b64
Adam Marshall <adamm3992@gmail.com>
2018-04-19 20:04:35 -0400

    rewrite apply_to_matches as Devel::Todo method

--- lib/Devel/Todo.pm
+++ lib/Devel/Todo.pm
@@ -54,8 +54,16 @@ sub has_elem {
   return (exists $he_self->{PROJECT}{contents}{$key});
 }
 
+sub has_sublist_elem {
+  my ($hse_self, $hse_subkey, $hse_key) = @_;
+  my $hse_list = $hse_self->{PROJECT}{contents};
+
+  return (exists $hse_list->{$hse_subkey}
+        && exists $hse_list->{$hse_subkey}{$hse_key});
+}
+
 # is scalar a todo list?
-sub isa_todo_list {
+sub isa_sublist {
   my ($val) = @_;
 
   return (ref($val) eq 'HASH' && exists $val->{contents}
@@ -71,7 +79,7 @@ sub Add_Element {
   my $ae_item = _ae_maker();
   for (@$ae_args) {
     if ($MOVE_ENABLED
-      && _apply_to_matches(\&_ae_mover, $ae_project, $_)) {
+      && apply_to_matches(\&_ae_mover, $ae_project, $_)) {
 
       next;
     }
@@ -94,7 +102,7 @@ sub Add_Element {
   return 0;
 }
 
-# passed to _apply_to_matches by add_element to move an item
+# passed to apply_to_matches by add_element to move an item
 sub _ae_mover {
   my ($project, $key) = @_;
 
@@ -129,46 +137,43 @@ sub _ae_maker {
 }
 
 # call a function on all relevant items
-sub _apply_to_matches {
-  my $sub   = shift;
-  my $start = shift;
-  my $key   = shift;
+sub apply_to_matches {
+  my ($atm_self, $atm_sub, $atm_key) = @_;
 
-  if (ref($key) eq 'ARRAY') {
+  if (ref($atm_key) eq 'ARRAY') {
     return 0 unless $atm_self->has_elem($atm_key->[0]);
 
-    my $count   = 0;
-    my $sublist = $start->{contents}{ $key->[0] };
+    my $atm_count   = 0;
+    my $atm_sublist = $atm_self->{PROJECT}{contents}{ $atm_key->[0] };
 
-    if (isa_todo_list($sublist)) {
-      foreach ($key->[1]) {
-        $count += _apply_to_matches($sub, $sublist, $_);
+    if (isa_sublist($atm_sublist)) {
+      foreach (@{ $atm_key->[1] }) {
+        if ($atm_self->has_sublist_elem($atm_key->[0], $_)) {
+          &{ $atm_sub }($atm_sublist, $atm_key);
+        }
       }
 
-      return $count;
-    }
-    else {
-      return 0;
+      return $atm_count;
     }
   }
-  else {
-    return 0 unless $atm_self->has_elem($atm_key);
-
-    &{ $sub }($start, $key);
+  elsif ($self->has_elem($atm_key)) {
+    &{ $atm_sub }($atm_self->{PROJECT}, $atm_key);
+    
+    return 1;
   }
 
-  return 1;
+  return 0;
 }
 
 # change relevant items 
 sub Edit_Element {
   my ($ee_file, $ee_project, $ee_args) = @_;
   
-  _error('list must have contents') unless (isa_todo_list($ee_project));
+  _error('list must have contents') unless (isa_sublist($ee_project));
   _error('cannot edit with \'all\' status') if ($STATUS eq 'all');
 
   foreach (@$ee_args) {
-    _apply_to_matches(\&_ee_set_attrs, $ee_project, $_);
+    apply_to_matches(\&_ee_set_attrs, $ee_project, $_);
   }
 
   DumpFile($ee_file, $ee_project);
@@ -176,7 +181,7 @@ sub Edit_Element {
   return 0;
 }
 
-# passed to _apply_to_matches by edit_element to change items
+# passed to apply_to_matches by edit_element to change items
 sub _ee_set_attrs {
   my ($list, $key) = @_;
 
@@ -210,13 +215,13 @@ sub _ee_set_attrs {
 sub Show_Element {
   my ($se_project, $se_args) = @_;
 
-  _error('nothing to show') unless (isa_todo_list($se_project));
+  _error('nothing to show') unless (isa_sublist($se_project));
 
   if (scalar @$se_args) {
     foreach (@$se_args) {
       # ignore arrayref args
       if (ref($_) eq '') {
-        _apply_to_matches(\&_se_dumper, $se_project, $_);
+        apply_to_matches(\&_se_dumper, $se_project, $_);
       }
     }
   }
@@ -239,7 +244,7 @@ sub _se_dumper {
     # print each item, excluding sublists, if it has the right status
     # apply the same rule to the ITEMS of a sublist
     while ((my ($k, $v) = each %{ $project->{contents} })) {
-      if (isa_todo_list($v)) {
+      if (isa_sublist($v)) {
         _se_dumper($project, $k);
       }
       elsif (_has_the_status($v, $STATUS)) {
@@ -250,7 +255,7 @@ sub _se_dumper {
   else { 
     my $sublist = $project->{contents}{$key};
 
-    return unless isa_todo_list($sublist);
+    return unless isa_sublist($sublist);
 
     while ((my ($k, $v) = each %{ $sublist->{contents} })) {
       if (_has_the_status($v, $STATUS)) {
@@ -270,12 +275,12 @@ sub _se_dumper {
 sub Delete_Element {
   my ($de_file, $de_data, $de_args) = @_;
 
-  _error('nothing to delete') unless (isa_todo_list($de_data));
+  _error('nothing to delete') unless (isa_sublist($de_data));
 
   my $de_contents = $de_data->{contents};
   if (scalar @$de_args) {
     foreach (@$de_args) {
-      _apply_to_matches(\&_de_deleter, $de_data, $_);
+      apply_to_matches(\&_de_deleter, $de_data, $_);
     }
   }
   else {
@@ -283,7 +288,7 @@ sub Delete_Element {
       if (_has_the_status($de_contents->{$key}, $STATUS)) {
         delete $de_contents->{$key};
       }
-      elsif (isa_todo_list($de_contents->{$key})) {
+      elsif (isa_sublist($de_contents->{$key})) {
         while ((my ($subkey, $subvalue) = each %{ $de_contents->{$key}{contents} })) {
           _de_deleter($de_contents->{$key}, $subkey);
         }
@@ -296,7 +301,7 @@ sub Delete_Element {
   return 0;
 }
 
-# passed to _apply_to_matches by delete_element to remove an item
+# passed to apply_to_matches by delete_element to remove an item
 sub _de_deleter {
   my ($project, $key) = @_;
 
